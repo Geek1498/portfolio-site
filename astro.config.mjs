0cdeb93fa2de367dfb5c6623672eee315ab723e3
Geek1498 <simbamasar4@gmail.com>
2026-03-30 13:06:45 +0200

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Geek1498.github.io',
-  base: '/portfolio-site/',
+  site: 'https://smasarakufa.online',
+  //base: '/portfolio-site/',
   trailingSlash: 'ignore',
 });
